c41212199487f11752fa3bdd430b7000a565f1fa
amiranm <amira.nagahmahrousbakr.xentra@gmail.com>
2024-08-02 20:10:11 +0800

    button update

--- js/button.js
+++ js/button.js
@@ -1,19 +1,14 @@
 $(document).ready(function() {
     const $navItems = $('.nav-item');
-    let lastClickedItem = null;
 
     const handleClick = ($item) => {
-        if (lastClickedItem && lastClickedItem !== $item[0]) {
-            $(lastClickedItem).removeClass('visible').addClass('hidden');
-        }
-
         $item.removeClass('hidden').addClass('visible');
-
-        lastClickedItem = $item[0];
     };
+
     $navItems.on('click', function() {
         const $item = $(this);
         handleClick($item);
     });
+
     $navItems.removeClass('hidden').addClass('visible');
 });
