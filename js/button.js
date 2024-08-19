e7f283e8aad4fc5a454fa4129d490fe787a637ae
amiranm <amira.nagahmahrousbakr.xentra@gmail.com>
2024-08-20 02:46:19 +0800

    update

--- js/button.js
+++ js/button.js
@@ -1,41 +1,54 @@
 $(document).ready(function() {
-    const $navItems = $('.nav-item');
-    
-    let lastClickedItem = null;
+  const $navItems = $('.nav-item');
+  const $logo = $('#logo');
+  let lastClickedItem = null;
 
-    $navItems.each(function() {
-        const $item = $(this);
-        
-        $item.on('click', function() {
-            const $firstItem = $('.nav-item').first();
-            
-            const firstItemRect = $firstItem[0].getBoundingClientRect();
+  function resetButtons() {
+      $navItems.each(function() {
+          const $nav = $(this);
+          $nav.removeClass('hidden returning');
+          $nav.css('transform', '');
+          $nav.removeClass('moved');
+      });
+      $logo.removeClass('moved');
+      $logo.css('transform', '');
+  }
 
-            if (lastClickedItem && lastClickedItem[0] === this) {
-                $navItems.each(function() {
-                    const $nav = $(this);
-                    $nav.removeClass('hidden');
-                    $nav.css('transform', '');
-                    $nav.removeClass('moved');
-                });
-                lastClickedItem = null;
-            } else {
-                const itemRect = this.getBoundingClientRect();
-                
-                const translateX = firstItemRect.left - itemRect.left;
-                const translateY = firstItemRect.top - itemRect.top;
+  function animateButton($item) {
+      const $firstItem = $('.nav-item').first();
+      const firstItemRect = $firstItem[0].getBoundingClientRect();
+      const itemRect = $item[0].getBoundingClientRect();
+      const translateX = firstItemRect.left - itemRect.left;
+      const translateY = firstItemRect.top - itemRect.top;
 
-                $navItems.each(function() {
-                    const $nav = $(this);
-                    if ($nav[0] !== $item[0]) {
-                        $nav.addClass('hidden');
-                    } else {
-                        $nav.addClass('moved');
-                        $nav.css('transform', `translate(${translateX}px, ${translateY}px)`);
-                    }
-                });
-                lastClickedItem = $item;
-            }
-        });
-    });
+      if ($item.is($logo)) {
+          // Logo button click
+          $logo.addClass('moved');
+          $logo.css('transform', 'rotate(360deg)');
+          resetButtons(); // Reset all other buttons
+          lastClickedItem = $logo;
+      } else {
+          // Other buttons click
+          if (lastClickedItem && lastClickedItem[0] === $item[0]) {
+              resetButtons();
+              lastClickedItem = null;
+          } else {
+              resetButtons();
+              $navItems.each(function() {
+                  const $nav = $(this);
+                  if ($nav[0] !== $item[0]) {
+                      $nav.addClass('hidden');
+                  } else {
+                      $nav.addClass('moved');
+                      $nav.css('transform', `translate(${translateX}px, ${translateY}px) rotate(360deg)`);
+                  }
+              });
+              lastClickedItem = $item;
+          }
+      }
+  }
+
+  $navItems.on('click', function() {
+      animateButton($(this));
+  });
 });
--- js/main.js
+++ js/main.js
@@ -1,39 +1,68 @@
 $(document).ready(function () {
-  AOS.init();
+    AOS.init();
 
-  var videos = $('video');
-  var videosLoaded = 0;
+    var videos = $('video');
+    var videosLoaded = 0;
+    let lastClickedButton = null;
+    let videoPlaybackState = {}; // 1 for played, 0 for not played
 
-  function checkVideosLoaded() {
-    videosLoaded++;
-    if(videosLoaded === videos.length) {
-      $('.preloader').addClass('hide');
+    function checkVideosLoaded() {
+        videosLoaded++;
+        if (videosLoaded === videos.length) {
+            $('.preloader').addClass('hide');
 
-      setTimeout(()=>{
-        $('.logo').addClass('visible');
-        $('#nav').addClass('visible');
-      }, 200);
+            setTimeout(() => {
+                $('.logo').addClass('visible');
+                $('#nav').addClass('visible');
+            }, 200);
+        }
     }
-  }
 
-  videos.each(function(){
-    var video = this;
-    video.onloadeddata = checkVideosLoaded;
-  });
-
-  $('.nav-item').click(function () {
-    var id = $(this).attr('id');
-    var currentSection = $('#section'+id);
-    var currentVideo =  videojs('#video'+id);
-
-    $('section').addClass('hide').removeClass('show').not(currentSection);
-    currentSection.addClass('show').removeClass('hide');
-    $('section.hide video').each(function(){
-      this.pause();
-      this.currentTime = 0;
+    videos.each(function () {
+        var video = this;
+        video.onloadeddata = checkVideosLoaded;
     });
-    currentVideo.play();
-  });
+
+    $('.nav-item').click(function () {
+        var id = $(this).attr('id');
+        var currentSection = $('#section' + id);
+        var currentVideo = videojs('#video' + id);
+
+        $('section').addClass('hide').removeClass('show').not(currentSection);
+        currentSection.addClass('show').removeClass('hide');
+
+        // Pause and reset all other videos
+        $('section.hide video').each(function () {
+            if (!this.ended) {
+                this.pause();
+                this.currentTime = 0;
+            }
+        });
+
+        if (videoPlaybackState[id] !== 1) {
+            currentVideo.one('ended', function () {
+                videoPlaybackState[id] = 0; 
+                
+                // Reset buttons and video state
+                $('.nav-item').each(function () {
+                    const $nav = $(this);
+                    $nav.removeClass('hidden returning');
+                    $nav.css('transform', '');
+                    $nav.removeClass('moved');
+                });
+                $('#logo').removeClass('moved');
+            });
+
+            currentVideo.play();
+            videoPlaybackState[id] = 1;
+        } else {
+            // If the video is already playing do not reset it
+            if (currentVideo.paused()) {
+                currentVideo.currentTime(0);
+                currentVideo.play();
+            }
+        }
+
+        lastClickedButton = $(this);
+    });
 });
-
-
